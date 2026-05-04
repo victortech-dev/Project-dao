6c87b48907e1aaab9275a94900f77f0fc831efdb
vkdev <dev.victorkaua@gmail.com>
2026-05-04 08:02:03 -0300

    Department and Seller

---
+++ src/modelo/DepartmentDao.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import model.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+     void insert(Department obj);
+     void update(Department obj);
+     void deleteById(Integer id);
+     Department finById(Integer id);
+
+     List<Department> findAll();
+}
